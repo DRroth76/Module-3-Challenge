e2b3c11617daa5207cb9837c700616ed52b0bd5a
DRroth76 <david.randall.roth97@gmail.com>
2023-11-16 12:14:16 -0600

    Module 3 Challenge results have been exported to a text file.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,77 @@
+# imports modules needed
+import csv
 
+# sets file path
+budget_csv = r'C:\Data Analysis Bootcamp\Module 3\cloned_module3challenge_files\Module-3-Challenge\PyBank\Resources\budget_data.csv'
+budget_txt = r'C:\Data Analysis Bootcamp\Module 3\cloned_module3challenge_files\Module-3-Challenge\PyBank\analysis\analysis_results.txt'
+
+# establish lists
+months = []
+money_amounts = []
+
+# to open the CSV file
+with open(budget_csv, 'r') as csvfile:
+    csvreader = csv.reader(csvfile,delimiter=",")
+    header = next(csvreader)
+    data = list(csvreader)
+
+# establishing variables
+    number_of_months = 0
+    sum_of_profit = 0
+    previous_money_amount = 0
+    change_in_profit = 0
+    total_change_in_profit = 0
+    month_of_max_increase = ''
+    month_of_max_decrease = ''
+    max_increase = 0
+    max_decrease = 0
+
+# for loop to pull data 
+    for row in data:
+        month = str(row[0])
+        money_amount = int(row[1])
+
+        months.append(month)
+        money_amounts.append(money_amount)
+
+        sum_of_profit += money_amount
+        number_of_months += 1
+        
+    # if statements to find total change in profit, max increase, and max decrease
+        if number_of_months > 1:
+            change_in_profit = money_amount - previous_money_amount
+            total_change_in_profit += change_in_profit
+        
+            if change_in_profit > max_increase:
+                max_increase = change_in_profit
+                month_of_max_increase = month
+
+            if change_in_profit < max_decrease:
+                max_decrease = change_in_profit
+                month_of_max_decrease = month
+        
+        previous_money_amount = money_amount
+
+    average_change = total_change_in_profit / (number_of_months - 1)
+
+# print financial analysis
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total Months: {number_of_months}")
+print(f"Total: ${sum_of_profit}")
+print(f"Average Change: ${round(average_change, 2)}")
+print(f"Greatest Increase in Profits: {month_of_max_increase} (${max_increase})")
+print(f"Greatest Decrease in Profits: {month_of_max_decrease} (${max_decrease})")
+
+# to print to text file
+with open(budget_txt, "w") as text_file:
+    output = (
+        "Financial Analysis\n"
+        "----------------------------\n"
+        f"Total Months: {number_of_months}\n"
+        f"Total: ${sum_of_profit}\n"
+        f"Average Change: ${round(average_change, 2)}\n"
+        f"Greatest Increase in Profits: {month_of_max_increase} (${max_increase})\n"
+        f"Greatest Decrease in Profits: {month_of_max_decrease} (${max_decrease})\n"
+        )
+    text_file.write(output)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,84 @@
+# imports modules needed
+import csv
 
+# sets file path
+election_csv = r'C:\Data Analysis Bootcamp\Module 3\cloned_module3challenge_files\Module-3-Challenge\PyPoll\Resources\election_data.csv'
+election_txt = r'C:\Data Analysis Bootcamp\Module 3\cloned_module3challenge_files\Module-3-Challenge\PyPoll\analysis\analysis_results.txt'
+
+# establish lists
+ballot_id = []
+county = []
+candidate = []
+
+# to open the CSV file
+with open(election_csv, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    header = next(csvreader)
+    data = list(csvreader)
+
+# establishing variables
+    total_votes = 0
+    stockham_vote_count = 0
+    stockham_percentage = 0
+    degette_vote_count = 0
+    degette_percentage = 0
+    doane_vote_count = 0
+    doane_percentage = 0
+    winner = ''
+
+# for loop to pull data
+    for row in data:
+        ballot_num = str(row[0])
+        candidate_name = str(row[2])
+
+        ballot_id.append(ballot_num)
+        candidate.append(candidate_name)
+
+# if statements to count votes
+        if candidate_name == "Charles Casper Stockham":
+            stockham_vote_count += 1
+        elif candidate_name == "Diana DeGette":
+            degette_vote_count += 1
+        elif candidate_name == "Raymon Anthony Doane":
+            doane_vote_count += 1
+    
+            total_votes = stockham_vote_count + degette_vote_count + doane_vote_count
+            stockham_percentage = round((stockham_vote_count / total_votes) * 100, 3)
+            degette_percentage = round((degette_vote_count / total_votes) * 100, 3)
+            doane_percentage = round((doane_vote_count / total_votes) * 100, 3)
+
+    
+# if statements to find the winner
+    if stockham_vote_count > degette_vote_count and doane_vote_count:
+        winner = "Charles Casper Stockham"
+    elif degette_vote_count > doane_vote_count:
+        winner = "Diana DeGette"
+    else:
+        winner = "Raymon Anthony Doane"
+# print results and winner
+print("Election Results")
+print("-------------------------")
+print(f"Total Votes: {total_votes}")
+print("-------------------------")
+print(f"Charles Casper Stockham: {stockham_percentage}% ({stockham_vote_count})")
+print(f"Diana DeGette: {degette_percentage}% ({degette_vote_count})")
+print(f"Raymon Anthony Doane: {doane_percentage}% ({doane_vote_count})")
+print("-------------------------")
+print(f"Winner: {winner}")
+print("-------------------------")
+
+# to print to text file
+with open(election_txt, "w") as text_file:
+    output = (
+       "Election Results\n"
+       "-------------------------\n"
+       f"Total Votes: {total_votes}\n"
+       "-------------------------\n"
+       f"Charles Casper Stockham: {stockham_percentage}% ({stockham_vote_count})\n"
+       f"Diana DeGette: {degette_percentage}% ({degette_vote_count})\n"
+       f"Raymon Anthony Doane: {doane_percentage}% ({doane_vote_count})\n"
+       "-------------------------\n"
+       f"Winner: {winner}\n"
+       "-------------------------\n"
+    )
+    text_file.write(output)
